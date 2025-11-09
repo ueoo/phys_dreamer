836136cbb773ba634e5b63edcd8ce88bfa19689b
Yue Gao <yuegao@cs.stanford.edu>
2025-11-09 02:51:49 -0800

    update

--- scripts/03_copy_point_cloud.py
+++ scripts/03_copy_point_cloud.py
@@ -1,16 +1,17 @@
 import os
 import shutil
 
+from tqdm import tqdm
+
 
 scene_names = ["cloth", "flower", "newton", "box", "bow"]
 
 
-for scene_name in scene_names:
+for scene_name in tqdm(scene_names):
     data_root = f"data_NeuROK_sim/{scene_name}_gs_iteration_30k"
-    point_cloud_path = f"{data_root}/point_cloud.ply"
-    moving_particles_path = f"{data_root}/moving_points.ply"
-
     images_dataset_dir = f"data_NeuROK_sim/{scene_name}_images"
 
-    shutil.copy(point_cloud_path, f"{images_dataset_dir}/point_cloud.ply")
-    shutil.copy(moving_particles_path, f"{images_dataset_dir}/moving_part_points.ply")
+    shutil.copy(f"{data_root}/point_cloud.ply", f"{images_dataset_dir}/point_cloud.ply")
+    shutil.copy(f"{data_root}/moving_points.ply", f"{images_dataset_dir}/moving_part_points.ply")
+    if os.path.exists(f"{data_root}/stem.ply"):
+        shutil.copy(f"{data_root}/stem.ply", f"{images_dataset_dir}/stem_points.ply")
